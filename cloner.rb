fee8b93a5df5ac488f5f325aa8ee24aa196f3486
Evan Sparkman <me@evansparkman.com>
2018-02-05 16:04:06 -0500

    Accept a comma seperated list of repos

--- cloner.rb
+++ cloner.rb
@@ -1,16 +1,21 @@
-repos = ['legacy_dashboards', 'listen360', 'models', 'migrations', 'feedback', 'einstein', 'dashboards', 'api_client']
+puts "What repos would you like to clone from Github?"
+puts "(ex: rails/rails, listen360/legacy_dashboards)"
+repos = gets.chomp
+
+repo_list = repos.split(', ')
 
 @home_dir = Dir.home
 
-def clone(repos)
+def clone(repo_list)
   set_working_dir
-  repos.each do |repo|
-    puts `git clone git@github.com:listen360/#{repo}.git`
+  repo_list.each do |repo|
+    puts `git clone git@github.com:#{repo}.git`
   end
 end
 
 def set_working_dir
   puts 'Please enter the directory you will be working from.'
+  puts '(this folder should live inside your Home folder'
   @working_dir = gets.chomp
 
   if Dir.exists?("#{@home_dir}/#{@working_dir}")
@@ -23,7 +28,6 @@ end
 
 def create_working_directory(working_dir)
   printf "\033[31mDirectory doesn't exist. Would you like to create it? Please type yes or no: \033[0m"
-  # prompt = STDIN.gets.chomp
   create_dir = STDIN.gets.chomp
   if create_dir == 'yes'
     Dir.mkdir(File.join(Dir.home, working_dir), 0700)
@@ -33,4 +37,4 @@ def create_working_directory(working_dir)
   end
 end
 
-clone(repos)
+clone(repo_list)
